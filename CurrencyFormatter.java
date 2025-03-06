fd123fb7b0651da336b1296aab709a36f2b37f87
prapti0 <123829919+prapti0@users.noreply.github.com>
2025-03-06 23:55:45 +0530

    Create CurrencyFormatter.java

---
+++ CurrencyFormatter.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.text.*;
+
+public class CurrencyFormatter{
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
